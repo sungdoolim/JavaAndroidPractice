a1dc7046c56e9a05edb0a5525f018b7c95f3d1d6
sungdoolim <sungdoolim@naver.com>
2021-09-12 17:16:34 +0900

    linearlayout / gravity / layout_weight etc practice

--- app/src/main/java/zx9/staris/aprtext/MainActivity.java
+++ app/src/main/java/zx9/staris/aprtext/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 화면을 자바로 구성하기
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        Button button1 = new Button(this);
+        button1.setText("Button1");
+        button1.setLayoutParams(params);
+        mainLayout.addView(button1);
+
+        setContentView(mainLayout);// 결국 위에서 만든 linearlayout을 보여준다
     }
+
     public void btnTestClick(View v){
         Toast.makeText(this,"토스트!",Toast.LENGTH_SHORT).show();
             Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
